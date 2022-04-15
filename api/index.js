9ff8b2ccd15805271ae33021cf8bffd954788618
Mike Delucia <mikey970@yahoo.com>
2022-04-15 17:04:08 -0400

    heroku deploy

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 6000;
+const MONGODB_URI = process.env.PORT || 6000;
 const bodyParser = require('body-parser');
 module.exports = app;
 
@@ -10,6 +10,6 @@ app.use(express.json());
 
 app.use('/api', require('./routes'));
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+app.listen(MONGODB_URI, () => {
+  console.log(`listening on port ${MONGODB_URI}`);
 })
