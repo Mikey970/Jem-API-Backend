6ae1c3f8aed2dfe852a93bd933b067f890660c64
Joseph <polanco.joseph1@gmail.com>
2022-04-19 10:26:36 -0400

    added route order-electronic

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use('/orders', require('./orders'));
 router.use('/electronics', require('./electronics'));
+router.use('/orderElectronics', require('./orderElectronics'));
 
 module.exports = router;
